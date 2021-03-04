7b9306c0cf8ff12322b514d459c9e2977cdf83da
Luis Hernández <53943021+luishernand@users.noreply.github.com>
2021-03-04 10:23:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import streamlit.components.v1 as stc
 
 @st.cache
 def load_dataset():
-	df = pd.read_excel('crypto_data.xlsx', sheet_name='data')
+	df = pd.read_excel('https://github.com/luishernand/crypto_analysis/blob/main/data/crypto_data.xlsx', sheet_name='data')
 	df.set_index('Date', inplace=True)
 	return df
 
